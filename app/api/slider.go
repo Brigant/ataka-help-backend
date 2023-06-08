8a7bfa7a3a5cd0534acaf795b54c1b98b419aa58
Yehor Tverytinov <etverya11@gmail.com>
2023-06-08 15:04:33 +0300

    fix: lint checks and response status corrections

--- app/api/slider.go
+++ app/api/slider.go
@@ -28,11 +28,11 @@ func NewSliderHandler(service SliderService, log *logger.Logger) Slider {
 }
 
 func (s Slider) getSlider(ctx *fiber.Ctx) error {
-
 	response, err := s.Service.ReturnSlider()
 	if err != nil {
 		s.log.Errorw("getSlider", "getSlider error", err.Error())
-		return ctx.JSON(structs.SetResponse(fiber.StatusInternalServerError, err.Error()))
+
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
 
 	result := structs.SliderResponse{
@@ -44,36 +44,43 @@ func (s Slider) getSlider(ctx *fiber.Ctx) error {
 }
 
 func (s Slider) createSlider(ctx *fiber.Ctx) error {
-
 	form, err := ctx.MultipartForm()
 	if err != nil {
-		return ctx.JSON(structs.SetResponse(fiber.StatusBadRequest, err.Error()))
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
 
 	title := form.Value["title"]
+
 	if title == nil || len(title[0]) < 4 || len(title[0]) > 300 {
 		s.log.Debugw("createSlider", "form.Value title", "title is blank or out of range limits")
-		return ctx.JSON(structs.SetResponse(fiber.StatusBadRequest, "title is blank or out of range limits"))
+
+		return fiber.NewError(fiber.StatusBadRequest, "title is blank or out of range limits")
 	}
 
 	file := form.File["thumb"]
+
 	if file != nil {
 		s.log.Debugw("createSlider", "file-name", file[0].Filename, "file-size", file[0].Size)
 	}
+
 	if file == nil || !isAllowedContentType(allowedContentType, file[0].Header["Content-Type"][0]) {
 		s.log.Debugw("createSlider", "form.File", err.Error())
-		return ctx.JSON(structs.SetResponse(fiber.StatusBadRequest, "thumb is absent"))
+
+		return fiber.NewError(fiber.StatusBadRequest, "thumb is absent")
 	}
 
 	size := file[0].Size
+
 	if size > 5*1024*1024 {
 		s.log.Debugw("createSlider", "form.File", "file too large")
-		return ctx.JSON(structs.SetResponse(fiber.StatusBadRequest, "file too large"))
+
+		return fiber.NewError(fiber.StatusBadRequest, "file too large")
 	}
 
 	if err := s.Service.SaveSlider(ctx.Context(), form); err != nil {
 		s.log.Errorw("createSlider", "createSlider error", err.Error())
-		return ctx.JSON(structs.SetResponse(fiber.StatusInternalServerError, err.Error()))
+
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
 
 	return ctx.JSON(structs.SetResponse(http.StatusOK, "success"))
