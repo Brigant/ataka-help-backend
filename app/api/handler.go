f3827b0b5693c73f50460b450e136d537e19547d
Brigant <sergeyjuly@gmail.com>
2023-06-11 23:42:07 +0300

    after review

--- app/api/handler.go
+++ app/api/handler.go
@@ -55,7 +55,6 @@ func isAllowedFileExtention(allowedList []string, fileName string) bool {
 	nameParts := strings.Split(fileName, ".")
 
 	fileExt := nameParts[len(nameParts)-1]
-	fmt.Println(fileExt)
 	for _, i := range allowedList {
 		if i == fileExt {
 			return true
